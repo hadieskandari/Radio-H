313febf376e684a39cb76ddb9db62c5cef9482f8
hadieskandari <hadieskandari01@gmail.com>
2023-04-29 02:04:37 +0330

    implement streaming queue for get tracks and broadcast

---
+++ src/@types/queue.d.ts
@@ -0,0 +1,21 @@
+export interface QueueInterface {
+    tracks: string[];
+    broadcast(chunk: Buffer): void;
+    addClient(): {
+        id: string;
+        client: PassThrough;
+    };
+    removeClient(id: string): void;
+    loadTracks(dir: string): Promise<void>;
+    getTrackBitrate(filepath: string): Promise<number>;
+    getNextTrack(): {
+        filepath: string;
+        bitrate: number;
+    };
+    pause(): void;
+    resume(): void;
+    started(): boolean;
+    play(useNewTrack?: boolean): void;
+    loadTrackStream(): void;
+    start(): Promise<void>;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,66 @@
 import http from "http";
 import path from "path";
+import fs from 'fs'
 
-import express, { Express, Response } from "express";
+import express, { Express, Request, Response } from "express";
 import { Server } from "socket.io";
+import queue from "./queue";
+
 
 const app: Express = express();
 const PORT = 3001;
 const server = http.createServer(app);
 const io = new Server(server);
 
-app.get("/", (_, res: Response) => {
-    res.sendFile(path.join(__dirname, "index.html"));
-});
+(async () => {
+    await queue.loadTracks("tracks");
+    queue.play();
 
-io.on('connection', (socket) => {
-    console.log('a user connected');
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
+    io.on("connection", (socket) => {
+        if (queue.bufferHeader) {
+            socket.emit("bufferHeader", queue.bufferHeader);
+        }
+
+        socket.on("bufferHeader", (header) => {
+            queue.bufferHeader = header;
+            socket.broadcast.emit("bufferHeader", queue.bufferHeader);
+        });
+
+        socket.on("stream", (packet) => {
+            // Only broadcast microphone if a header has been received
+            if (!queue.bufferHeader) return;
+
+            // Audio stream from host microphone
+            socket.broadcast.emit("stream", packet);
+        });
+
+        socket.on("control", (command) => {
+            switch (command) {
+                case "pause":
+                    queue.pause();
+                    break;
+                case "resume":
+                    queue.resume();
+                    break;
+            }
+        });
     });
-    socket.on('chat message', (msg) => {
-        console.log(msg);
-        io.emit('chat message', msg);
+    app.get("/stream", (req, res) => {
+        const { id, client } = queue.addClient();
+
+        res.set({
+            "Content-Type": "audio/mp3",
+            "Transfer-Encoding": "chunked",
+        }).status(200);
+
+        client.pipe(res);
+
+        req.on("close", () => {
+            queue.removeClient(id);
+        });
     });
 
-});
-
-server.listen(PORT, () => {
-    console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
-});
+    server.listen(PORT, () => {
+        console.log(`Listening on port ${PORT}`);
+    });
+})();
