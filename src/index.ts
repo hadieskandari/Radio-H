ea45eefaa106f4a9810bc82b9d6cb72d2ccd460f
hadieskandari <hadieskandari01@gmail.com>
2023-04-28 05:06:25 +0330

    first commit project setup

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import http from "http";
 import path from "path";
 
-import express, {Express, Response} from "express";
-import {Server} from "socket.io";
+import express, { Express, Response } from "express";
+import { Server } from "socket.io";
 
 const app: Express = express();
 const PORT = 3001;
@@ -10,21 +10,21 @@ const server = http.createServer(app);
 const io = new Server(server);
 
 app.get("/", (_, res: Response) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+    res.sendFile(path.join(__dirname, "index.html"));
 });
 
 io.on('connection', (socket) => {
-  console.log('a user connected');
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
-  });
-  socket.on('chat message', (msg) => {
-    console.log(msg);
-    io.emit('chat message', msg);
-  });
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+    socket.on('chat message', (msg) => {
+        console.log(msg);
+        io.emit('chat message', msg);
+    });
 
 });
 
 server.listen(PORT, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
+    console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
 });
